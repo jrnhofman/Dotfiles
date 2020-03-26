48a6ec77e9a10bd2ab6a935164ccaeb2fa65a530
Jeroen Hofman <jrn.hofman@gmail.com>
2020-03-26 11:59:32 +0100

    Added settings for vscode.

--- set_sym_links.sh
+++ set_sym_links.sh
@@ -3,7 +3,11 @@
 cd dotfiles
 FILES=".[^.]*"
 for f in $FILES; do
+	rm $HOME/$f
 	ln -s $PWD/$f $HOME/$f
 done
+
+rm $HOME/repos/general.code-workspace
+ln -s $PWD/general.code-workspace $HOME/repos/
 
 cd ..
